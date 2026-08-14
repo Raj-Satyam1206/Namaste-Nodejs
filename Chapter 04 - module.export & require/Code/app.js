d092e31b111f2d7d27b49e4e02b34702edb5268b
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2026-08-14 14:33:37 +0530

    Main App module

--- Chapter 04 - module.export & require/Code/app.js
+++ Chapter 04 - module.export & require/Code/app.js
@@ -1,13 +1,17 @@
 // const { calculateMultiply } = require("./calulate/multiply");
 // const { x, calculateSum } = require("./calulate/sum");
-const { calculateMultiply, calculateSum } = require("./calulate/index")
+
+
+const { calcsum, calcmultiply } = require("./calulate")
+
 const data = require("./data.json")
 // import { calculateSum } from "./sum.js"
 var a = 10;
 var b = 20;
 console.log(a)
-calculateSum(a, b);
-calculateMultiply(a, b)
+
+calcsum(a, b);
+calcmultiply(a, b)
+
 console.log(data.name)
 console.log(data.city)
-// console.log(x)
